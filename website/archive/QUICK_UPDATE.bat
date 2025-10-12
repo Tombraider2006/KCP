5f91f1dfbecd173355baa321158f21253f66566c
TomBRaider2006 <utolyana@ya.ru>
2025-10-12 17:39:26 +0300

    Archive: Source code moved to private repository. Releases available at tomich.fun

--- website/archive/QUICK_UPDATE.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d D:\3DC\website
-scp public/feature.html root@tomich.fun:/opt/website/public/
-ssh root@tomich.fun "cd /opt/website && docker compose restart"
-echo DONE! Check https://tomich.fun/features/telegram
-pause
-
-
-
--- website/archive/UPLOAD_SETTINGS.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d D:\3DC\website
-echo Uploading settings.html...
-scp public/admin/settings.html root@tomich.fun:/opt/website/public/admin/
-echo Done! Reload https://tomich.fun/admin/settings.html
-pause
-
-
-
